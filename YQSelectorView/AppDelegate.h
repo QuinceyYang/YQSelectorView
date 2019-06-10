6d32d696a885dc3059c0bcbdb96d13b326947990
QuinceyYang <yqing555@qq.com>
2019-06-10 15:23:53 +0800

    Initial Commit

---
+++ YQSelectorView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQSelectorView
+//
+//  Created by 杨清 on 2019/6/10.
+//  Copyright © 2019 QuinceyYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YQSelectorView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQSelectorView
+//
+//  Created by 杨清 on 2019/6/10.
+//  Copyright © 2019 QuinceyYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
