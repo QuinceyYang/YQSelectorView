7471dfa2f0b5b702880e6a85a2c0300c87b6eeb8
QuinceyYang <yqing555@qq.com>
2019-06-11 11:28:39 +0800

    yqing:optimize

--- YQSelectorView/ViewController.m
+++ YQSelectorView/ViewController.m
@@ -22,7 +22,7 @@
     NSArray *typesArr = @[@"住宅",@"写字楼",@"公共机构",@"商业",@"商住两用",@"商写两用",@"旅游景点"];
     UIImage *icon = [UIImage imageNamed:@"icon_scorePay"];
     NSArray *iconArr = @[icon,icon,icon,icon,icon,icon];
-    YQButton *btn = [[YQButton alloc] initWithFrame:CGRectMake(0, 66, 200, 100)];
+    YQButton *btn = [[YQButton alloc] initWithFrame:CGRectMake(0, 66, 250, 100)];
     btn.backgroundColor = UIColor.greenColor;
     [btn setTitle:@"YQSelectorView show" forState:UIControlStateNormal];
     //btn.center = self.view.center;
@@ -34,13 +34,34 @@
         [self.view.window addSubview:view];
     };
     //
-    YQButton *btn2 = [[YQButton alloc] initWithFrame:CGRectMake(0, 175, 200, 100)];
+    YQButton *btn2 = [[YQButton alloc] initWithFrame:CGRectMake(0, 175, 250, 100)];
     btn2.backgroundColor = UIColor.greenColor;
     [btn2 setTitle:@"YQSelectorView show 2" forState:UIControlStateNormal];
     //btn.center = self.view.center;
     [self.view addSubview:btn2];
     btn2.tapAction = ^(YQButton *sender) {
         YQSelectorView *view = [YQSelectorView selectorViewWithFrame:UIScreen.mainScreen.bounds title:@"选择停车场类型" iconArr:iconArr textArray:typesArr imageOfSelected:[UIImage imageNamed:@"icon_duigou"] selectIndex:1 completion:^(NSInteger selectedIndex, NSString * _Nonnull selectedString) {
+            //
+        }];
+        view.isAutoCloseWhenSelected = NO;
+        [view setItemsTextAlignment:NSTextAlignmentLeft];
+        [self.view.window addSubview:view];
+    };
+    //
+    YQButton *btn3 = [[YQButton alloc] initWithFrame:CGRectMake(0, 285, 250, 100)];
+    btn3.backgroundColor = UIColor.greenColor;
+    [btn3 setTitle:@"YQSelectorView attribute" forState:UIControlStateNormal];
+    //btn.center = self.view.center;
+    [self.view addSubview:btn3];
+    btn3.tapAction = ^(YQButton *sender) {
+        NSString *str = @"商住两用";
+        NSMutableAttributedString *attr = [[NSMutableAttributedString alloc] initWithString:str];
+        [attr addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:17] range:[str rangeOfString:str]];
+        [attr addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:13] range:[str rangeOfString:@"两用"]];
+        [attr addAttribute:NSForegroundColorAttributeName value:[UIColor blueColor] range:[str rangeOfString:str]];
+        [attr addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:[str rangeOfString:@"商住"]];
+        NSArray *attrArr = [NSArray arrayWithObjects:attr,attr,attr,attr,nil];
+        YQSelectorView *view = [YQSelectorView selectorViewWithFrame:UIScreen.mainScreen.bounds title:@"选择停车场类型" iconArr:iconArr attributedTextArray:attrArr imageOfSelected:[UIImage imageNamed:@"icon_duigou"] selectIndex:1 completion:^(NSInteger selectedIndex, NSString * _Nonnull selectedString) {
             //
         }];
         view.isAutoCloseWhenSelected = NO;
